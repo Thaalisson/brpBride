102a520a22e3f7abe3ff5c8d1960525963610ef4
Thalisson <thalisson_21@icloud.com>
2024-06-27 14:33:50 -0400

    changes

--- src/api/leagueAPI.js
+++ src/api/leagueAPI.js
@@ -4,7 +4,7 @@ const API_KEY = process.env.REACT_APP_LOL_API_KEY;
 
 export const fetchPUUID = async (gameName, tagLine) => {
   try {
-    const response = await fetch(`https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}?api_key=${API_KEY}`);
+    const response = await fetch(`/riot/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}?api_key=${API_KEY}`);
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
@@ -18,7 +18,7 @@ export const fetchPUUID = async (gameName, tagLine) => {
 
 export const fetchSummonerByPUUID = async (puuid, region) => {
   try {
-    const response = await fetch(`https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}?api_key=${API_KEY}`);
+    const response = await fetch(`/br1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}?api_key=${API_KEY}`);
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
@@ -32,7 +32,7 @@ export const fetchSummonerByPUUID = async (puuid, region) => {
 
 export const fetchRankData = async (summonerId, region) => {
   try {
-    const response = await fetch(`https://${region}.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerId}?api_key=${API_KEY}`);
+    const response = await fetch(`/br1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerId}?api_key=${API_KEY}`);
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
