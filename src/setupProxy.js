df14a09f375bb67b0a0cb8ee6ff43f438e8dc6ba
Thalisson <42617043+Thaalisson@users.noreply.github.com>
2024-06-27 14:08:52 -0400

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/riot',
+    '',
     createProxyMiddleware({
       target: 'https://americas.api.riotgames.com',
       changeOrigin: true,
@@ -13,7 +13,7 @@ module.exports = function(app) {
     })
   );
   app.use(
-    '/br1',
+    '',
     createProxyMiddleware({
       target: 'https://br1.api.riotgames.com',
       changeOrigin: true,
