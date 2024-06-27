18953fdf234baeeccf6c40af85305a044e5a1c43
Thalisson <thalisson_21@icloud.com>
2024-06-27 16:57:13 -0400

    aee

--- src/api/leagueAPI.jsx
+++ src/api/leagueAPI.jsx
@@ -1,8 +1,6 @@
-const API_KEY = process.env.REACT_APP_LOL_API_KEY;
-
 export const fetchPUUID = async (gameName, tagLine) => {
   try {
-    const url = `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${encodeURIComponent(gameName)}/${encodeURIComponent(tagLine)}?api_key=${API_KEY}`;
+    const url = `/riot/account/v1/accounts/by-riot-id/${encodeURIComponent(gameName)}/${encodeURIComponent(tagLine)}`;
     console.log('Fetching PUUID from URL:', url);
     const response = await fetch(url);
     if (!response.ok) {
@@ -19,7 +17,7 @@ export const fetchPUUID = async (gameName, tagLine) => {
 
 export const fetchSummonerByPUUID = async (puuid) => {
   try {
-    const url = `https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}?api_key=${API_KEY}`;
+    const url = `/br1/lol/summoner/v4/summoners/by-puuid/${puuid}`;
     console.log('Fetching Summoner by PUUID from URL:', url);
     const response = await fetch(url);
     if (!response.ok) {
@@ -36,7 +34,7 @@ export const fetchSummonerByPUUID = async (puuid) => {
 
 export const fetchRankData = async (summonerId) => {
   try {
-    const url = `https://br1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerId}?api_key=${API_KEY}`;
+    const url = `/br1/lol/league/v4/entries/by-summoner/${summonerId}`;
     console.log('Fetching Rank Data from URL:', url);
     const response = await fetch(url);
     if (!response.ok) {
