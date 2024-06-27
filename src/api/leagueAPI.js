bdcc2c05207404f9b705d9d6e8aa2b75e5ec9d29
Thalisson <thalisson_21@icloud.com>
2024-06-27 14:22:06 -0400

    fixed proxy

--- src/api/leagueAPI.js
+++ src/api/leagueAPI.js
@@ -1,19 +1,19 @@
 const API_KEY = process.env.REACT_APP_LOL_API_KEY;
 
 export const fetchPUUID = async (gameName, tagLine) => {
-  const response = await fetch(`/riot/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}?api_key=${API_KEY}`);
+  const response = await fetch(`https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}?api_key=${API_KEY}`);
   const data = await response.json();
   return data;
 };
 
-export const fetchSummonerByPUUID = async (puuid) => {
-  const response = await fetch(`/br1/lol/summoner/v4/summoners/by-puuid/${puuid}?api_key=${API_KEY}`);
+export const fetchSummonerByPUUID = async (puuid, region) => {
+  const response = await fetch(`https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}?api_key=${API_KEY}`);
   const data = await response.json();
   return data;
 };
 
-export const fetchRankData = async (summonerId) => {
-  const response = await fetch(`/br1/lol/league/v4/entries/by-summoner/${summonerId}?api_key=${API_KEY}`);
+export const fetchRankData = async (summonerId, region) => {
+  const response = await fetch(`https://${region}.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerId}?api_key=${API_KEY}`);
   const data = await response.json();
   return data;
 };
