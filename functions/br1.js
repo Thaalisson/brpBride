dd49fbff7d48ebf794b5dcf6c8fbacc9f3e41753
Thalisson <thalisson_21@icloud.com>
2024-07-04 09:01:48 -0400

    changes

--- functions/br1.js
+++ functions/br1.js
@@ -1,21 +1,25 @@
-const fetch = require('node-fetch');
-
+// Adicionando esta linha para importar dynamicamente o fetch
 exports.handler = async function(event, context) {
-  const API_KEY = process.env.RIOT_API_KEY; // Defina a chave API no arquivo .env
-  const apiUrl = `https://br1.api.riotgames.com${event.path.replace('/.netlify/functions/br1', '')}?api_key=${API_KEY}`;
+    const fetch = (await import('node-fetch')).default;
   
-  try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data),
-    };
-  } catch (error) {
-    console.error('Error fetching:', error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: error.toString() }),
-    };
-  }
-};
+    console.log('Request received:', event.path);
+    const API_KEY = process.env.RIOT_API_KEY; // Defina a chave API no arquivo .env
+    const apiUrl = `https://br1.api.riotgames.com${event.path.replace('/.netlify/functions/br1', '')}?api_key=${API_KEY}`;
+    console.log('Fetching from:', apiUrl);
+  
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data),
+      };
+    } catch (error) {
+      console.error('Error fetching:', error);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: error.toString() }),
+      };
+    }
+  };
+  
--- functions/riot.js
+++ functions/riot.js
@@ -1,21 +1,25 @@
-const fetch = require('node-fetch');
-
+// Adicionando esta linha para importar dynamicamente o fetch
 exports.handler = async function(event, context) {
-  const API_KEY = process.env.RIOT_API_KEY; // Defina a chave API no arquivo .env
-  const apiUrl = `https://americas.api.riotgames.com${event.path.replace('/.netlify/functions/riot', '')}?api_key=${API_KEY}`;
+    const fetch = (await import('node-fetch')).default;
   
-  try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data),
-    };
-  } catch (error) {
-    console.error('Error fetching:', error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: error.toString() }),
-    };
-  }
-};
+    console.log('Request received:', event.path);
+    const API_KEY = process.env.RIOT_API_KEY; // Defina a chave API no arquivo .env
+    const apiUrl = `https://americas.api.riotgames.com${event.path.replace('/.netlify/functions/riot', '')}?api_key=${API_KEY}`;
+    console.log('Fetching from:', apiUrl);
+  
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data),
+      };
+    } catch (error) {
+      console.error('Error fetching:', error);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: error.toString() }),
+      };
+    }
+  };
+  
