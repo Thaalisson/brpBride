9ea8b229df3dadc1cc5ec20d595bd2db87778b03
Thalisson <thalisson_21@icloud.com>
2024-07-03 21:18:34 -0400

    changes

--- functions/riot-proxy.js
+++ functions/riot-proxy.js
@@ -1,37 +1,42 @@
-const express = require('express');
-const cors = require('cors');
+const fetch = require('node-fetch');
 
-const app = express();
-app.use(cors());
+exports.handler = async function(event, context) {
+  const API_KEY = 'RGAPI-721d309c-5851-44c1-af4c-ca58a0c58664';  // Use a sua API_KEY aqui
 
-const API_KEY = REACT_APP_LOL_API_KEY;  // Use a sua API_KEY aqui
+  const path = event.path.replace('/.netlify/functions/riot-proxy', '');
+  let apiUrl;
+  
+  if (path.includes('/riot')) {
+    apiUrl = `https://americas.api.riotgames.com${path.replace('/riot', '')}?api_key=${API_KEY}`;
+  } else if (path.includes('/br1')) {
+    apiUrl = `https://br1.api.riotgames.com${path.replace('/br1', '')}?api_key=${API_KEY}`;
+  } else {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: 'Invalid path' })
+    };
+  }
 
-app.get('/riot/*', async (req, res) => {
+  console.log('Fetching from:', apiUrl);
+
   try {
-    const fetch = (await import('node-fetch')).default;
-    const apiUrl = `https://americas.api.riotgames.com${req.path.replace('/riot', '')}?api_key=${API_KEY}`;
-    console.log('Fetching from:', apiUrl);
-    const response = await fetch(apiUrl);
-    res.json(await response.json());
+    const response = await fetch(apiUrl, {
+      method: event.httpMethod,
+      headers: { ...event.headers },
+      body: event.body
+    });
+
+    const data = await response.json();
+
+    return {
+      statusCode: response.status,
+      body: JSON.stringify(data)
+    };
   } catch (error) {
     console.error('Error fetching:', error);
-    res.status(500).json({ error: error.toString() });
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed to fetch data from Riot API' })
+    };
   }
-});
-
-app.get('/br1/*', async (req, res) => {
-  try {
-    const fetch = (await import('node-fetch')).default;
-    const apiUrl = `https://br1.api.riotgames.com${req.path.replace('/br1', '')}?api_key=${API_KEY}`;
-    console.log('Fetching from:', apiUrl);
-    const response = await fetch(apiUrl);
-    res.json(await response.json());
-  } catch (error) {
-    console.error('Error fetching:', error);
-    res.status(500).json({ error: error.toString() });
-  }
-});
-
-app.listen(4000, function () {
-  console.log("Server started on port 4000");
-});
+};
